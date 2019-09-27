bcdc9a137d28ab9f38a938cf77d186a78fdbbe80
Aleksandr Berlin <aleksanderberlin@gmail.com>
2019-09-27 15:39:25 +0300

    z6

--- lesson1/lesson1z3.c
+++ lesson1/lesson1z3.c
@@ -1,5 +1,5 @@
 //
-//  lesson1z3.c
+//  less1z2.c
 //  lesson1
 //
 //  Created by Александр Берлин on 27/09/2019.
@@ -7,20 +7,15 @@
 //
 
 #include <stdio.h>
-#include <math.h>
 
 int main(int argc, char* argv[]) {
-    float a, b, c, x1, x2;
-    printf("Enter a, b and c:\n");
-    scanf("%f %f %f", &a, &b, &c);
-    float d = b * b - 4 * a * c;
-    if (d > 0) {
-        x1 = (-b + sqrt(d)) / (2 * a);
-        x2 = (-b - sqrt(d)) / (2 * a);
-        printf("Equation has 2 roots: %f and %f\n", x1, x2);
-    } else if (d == 0) {
-        x1 = -b / (2 * a);
-        printf("Equation has 1 root: %f\n", x1);
-    } else printf("Equation has no roots.\n");
+    int a, b;
+    printf("Enter A and B:\n");
+    scanf("%d %d", &a, &b);
+    printf("A = %d, B = %d\n", a, b);
+    a = a ^ b;
+    b = b ^ a;
+    a = a ^ b;
+    printf("A = %d, B = %d\n", a, b);
     return 0;
 }
